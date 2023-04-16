693c234e81d107e1c5ad9c27f2dbc374937768c8
nourahmed <105996059+nour-elhabbal@users.noreply.github.com>
2023-04-16 20:29:02 +0200

    FIx

--- interactive-pricing-component-main/app.js
+++ interactive-pricing-component-main/app.js
@@ -12,19 +12,19 @@ const yearlyPricing = () => {
 
   if (inputValue < 20) {
     result.textContent = `50K PAGEVIEWS`;
-    pricing.textContent = `$ ${72} /year`;
+    pricing.innerHTML = `<span class="price">$ 72</span><span class="perYearOrMonth"> /Year</span>`;
   } else if (inputValue >= 20 && inputValue < 40) {
     result.textContent = `50K PAGEVIEWS`;
-    pricing.textContent = `$ ${108} /year`;
+    pricing.innerHTML = `<span class="price">$ 108</span><span class="perYearOrMonth"> /Year</span>`;
   } else if (inputValue >= 40 && inputValue < 60) {
     result.textContent = `100K PAGEVIEWS`;
-    pricing.textContent = `$ ${144} /year`;
+    pricing.innerHTML = `<span class="price">$ 144</span><span class="perYearOrMonth"> /Year</span>`;
   } else if (inputValue >= 60 && inputValue < 80) {
     result.textContent = `500K PAGEVIEWS`;
-    pricing.textContent = `$ ${216} /year`;
+    pricing.innerHTML = `<span class="price">$ 216</span><span class="perYearOrMonth"> /Year</span>`;
   } else if (inputValue >= 80) {
     result.textContent = `1M PAGEVIEWS`;
-    pricing.textContent = `$ ${324} /year`;
+    pricing.innerHTML = `<span class="price">$ 324</span><span class="perYearOrMonth"> /Year</span>`;
   }
 };
 
@@ -33,26 +33,26 @@ const monthlyPricing = () => {
 
   if (inputValue < 20) {
     result.textContent = `10K PAGEVIEWS`;
-    pricing.textContent = `$ ${(price.textContent = `8`)} /Month`;
+    pricing.innerHTML = `<span class="price">$ 8</span><span class="perYearOrMonth"> /Month</span>`;
   } else if (inputValue >= 20 && inputValue < 40) {
     result.textContent = `50K PAGEVIEWS`;
-    pricing.textContent = `$ ${12} /Month`;
+    pricing.innerHTML = `<span class="price">$ 12</span><span class="perYearOrMonth"> /Month</span>`;
   } else if (inputValue >= 40 && inputValue < 60) {
     result.textContent = `100K PAGEVIEWS`;
-    pricing.textContent = `$ ${16} /Month`;
+    pricing.innerHTML = `<span class="price">$ 16</span><span class="perYearOrMonth"> /Month</span>`;
   } else if (inputValue >= 60 && inputValue < 80) {
     result.textContent = `500K PAGEVIEWS`;
-    pricing.textContent = `$ ${24} /Month`;
+    pricing.innerHTML = `<span class="price">$ 24</span><span class="perYearOrMonth"> /Month</span>`;
   } else if (inputValue >= 80) {
     result.textContent = `1M PAGEVIEWS`;
-    pricing.textContent = `$ ${36} /Month`;
+    pricing.innerHTML = `<span class="price">$ 36</span><span class="perYearOrMonth"> /Month</span>`;
   }
 };
 
 // SCRIPT :---
 window.onload = () => {
   result.textContent = `100K PAGEVIEWS`;
-  price.textContent = `$ ${16} /Month`;
+  pricing.innerHTML = `<span class="price">$ 16</span><span class="perYearOrMonth"> /Month</span>`;
 };
 
 input.addEventListener("input", () => {
